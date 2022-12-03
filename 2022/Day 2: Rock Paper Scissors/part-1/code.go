ea79d4067dac88ff5ad1f5b524edc2253da8e288
Reza Shiri <rezashiri@cafebazaar.ir>
2022-12-03 12:52:13 +0330

    Fix part-2 day-2

--- 2022/Day 2: Rock Paper Scissors/part-1/code.go
+++ 2022/Day 2: Rock Paper Scissors/part-1/code.go
@@ -34,39 +34,6 @@ func scoreMove(x rune) int {
 	return 3
 }
 
-func myMove(oppMove rune, result rune) rune {
-	if result == 'Z' {
-		if oppMove == 'A' {
-			return 'Y'
-		}
-		if oppMove == 'B' {
-			return 'Z'
-		}
-
-		return 'X'
-	} else if result == 'X' {
-		if oppMove == 'A' {
-			return 'Z'
-		}
-		if oppMove == 'B' {
-			return 'X'
-		}
-
-		return 'Y'
-	} else {
-		if oppMove == 'A' {
-			return 'X'
-		}
-		if oppMove == 'B' {
-			return 'Y'
-		}
-
-		return 'Z'
-	}
-
-	return oppMove
-}
-
 func main() {
 	bytesRead, _ := ioutil.ReadFile("input.txt")
 	fileContent := string(bytesRead)
@@ -74,10 +41,8 @@ func main() {
 
 	var score int
 	for _, line := range lines {
-		first, res := rune(line[0]), rune(line[2])
-		second := myMove(first, res)
+		first, second := rune(line[0]), rune(line[2])
 		score += win(first, second) + scoreMove(second)
-		// fmt.Println(score)
 	}
 
 	fmt.Println(score)
